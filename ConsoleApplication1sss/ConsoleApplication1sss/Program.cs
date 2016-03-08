d82b21ea854522a19c3fc93b3e701253d39a4c91
duje13 <dujesol@gmail.com>
2016-03-08 09:04:14 +0100

    Update Program.cs
    dodana linija

--- ConsoleApplication1sss/ConsoleApplication1sss/Program.cs
+++ ConsoleApplication1sss/ConsoleApplication1sss/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1sss
         {
             Console.WriteLine("yads");
             Console.WriteLine("sdf");
+            Console.WriteLine("sdfsdf");
         }
     }
 }
