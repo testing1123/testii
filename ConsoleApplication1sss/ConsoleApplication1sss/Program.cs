0054686a962248b485ecc20bad7e10cf07c9c784
student <student@B526-09.fesb.hr>
2016-03-08 08:41:15 +0100

    PRVA VERZIJA

---
+++ ConsoleApplication1sss/ConsoleApplication1sss/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1sss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
